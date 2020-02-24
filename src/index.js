a4b266abd7be43288bcdf1b85a7c0110735e0d53
alexmeier2828 <ameier42@gmail.com>
2020-02-24 14:27:06 -0500

    added a simple prompt with a selectable choice

--- src/index.js
+++ src/index.js
@@ -1,1 +1,20 @@
-console.log('hello world')
+var prompt = require('prompt');
+var inquirer = require('inquirer');
+
+//I know we said we would use prompt, but I couldn't find a way to have slectable choices
+questions = [
+	{
+		type: 'list',
+		name: 'packageManager',
+		message: 'Choose a package manager: ',
+		choices: ['npm', 'yarn'],
+		default: 'npm'
+	}
+];
+
+
+inquirer
+	.prompt(questions)
+	.then(answers => {
+		console.log(answers)
+	});
