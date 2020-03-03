78e95e225cedd438e7a578594fadfda97ca1210b
CrazyCreje <cvalenti312@gmail.com>
2020-03-03 11:49:09 -0500

    Check user's installation for git, npm, and yarn, add shelljs

--- src/index.js
+++ src/index.js
@@ -1,7 +1,25 @@
-const { prompt } = require('enquirer');
+const { prompt } = require('enquirer')
+const shell = require('shelljs')
 const Enquirer = require('enquirer')
 
-questions = [
+if (!shell.which('git')) {
+	shell.echo("It appears that you don't have git installed. Before running web-starter-cli,\n"
+	+ "please install git and add it to your PATH.")
+	shell.exit(1);
+}
+
+const npm = shell.which("npm"); // returns null if npm isn't installed or on path
+const yarn = shell.which("yarn");
+
+if (!npm && !yarn) {
+	shell.echo("It appears that you don't have npm or yarn installed. Before running web-starter-cli,\n"
+	+ "please install npm or yarn and add it to your PATH.")
+	shell.exit(1);
+}
+
+// add welcome message
+
+questions = [ // need to eliminate options based on {npm} and {yarn}
 	{
 		type: 'Select',
 		name: 'packageManager',
