19d091a039009d6b27a284bbd54fbbd7dd981b76
CrazyCreje <cvalenti312@gmail.com>
2020-03-20 09:33:33 -0400

    add failing test

--- src/index.js
+++ src/index.js
@@ -3,64 +3,75 @@ const shell = require('shelljs')
 const Enquirer = require('enquirer')
 const chalk = require('chalk')
 
+const npm = shell.which("npm") // returns null if npm isn't installed or on path
+const yarn = shell.which("yarn")
+
+
+console.log(chalk.magenta("Welcome to web-starter-cli!\nThis program is designed to make your web-development experience easier."))
+console.log(chalk.magenta("It will create a Git repository, install dependencies, and generate boilerplate\nbased on your chosen frameworks."))
+
+questions = [ // need to eliminate options based on {npm} and {yarn}
+	{
+		type: 'Select',
+		name: 'packageManager',
+		message: 'Choose a package manager: ',
+		choices: [
+			{name: 'npm', disabled: !npm}, 
+			{name: 'yarn', disabled: !yarn}
+		],
+		default: 'npm'
+	},
+	{
+		type: 'input',
+		name: 'project',
+		message: 'What is the name of your project?'
+	},
+	{
+		type: 'Select',
+		name: 'frontend',
+		message: 'Choose a frontend framework: ',
+		choices: ['React', 'Angular', 'Vue'],
+		default: 'React'
+	},
+	{
+		type: 'Select',
+		name: 'backend',
+		message: 'Choose a backend framework: ',
+		choices: ['Express', 'Django', 'Flask'],
+		default: 'Express'
+	}
+
+];
+//Checks for CRTL-C and exits with goodbye message
+let answers = async () => {
+	try {
+		await prompt(questions)
+	} catch (err) {
+		console.log(chalk.yellow("Closing web-stater-cli..."))
+		shell.exit(0)
+	}
+}
+// Checks user's installation of git, npm, and yarn
+export const checkConfig = () => {
 	if (!shell.which('git')) {
 		shell.echo("It appears that you don't have git installed. Before running web-starter-cli,\n"
 		+ "please install git and add it to your PATH.")
 		shell.exit(1)
 	}
 
-	const npm = shell.which("npm") // returns null if npm isn't installed or on path
-	const yarn = shell.which("yarn")
-
 	if (!npm && !yarn) {
 		shell.echo("It appears that you don't have npm or yarn installed. Before running web-starter-cli,\n"
 		+ "please install npm or yarn and add it to your PATH.")
 		shell.exit(1)
 	}
+	return 0
+}
 
-	console.log(chalk.magenta("Welcome to web-starter-cli!\nThis program is designed to make your web-development experience easier."))
-	console.log(chalk.magenta("It will create a Git repository, install dependencies, and generate boilerplate\nbased on your chosen frameworks."))
-
-	questions = [ // need to eliminate options based on {npm} and {yarn}
-		{
-			type: 'Select',
-			name: 'packageManager',
-			message: 'Choose a package manager: ',
-			choices: [
-				{name: 'npm', disabled: !npm}, 
-				{name: 'yarn', disabled: !yarn}
-			],
-			default: 'npm'
-		},
-		{
-	        type: 'input',
-	        name: 'project',
-	        message: 'What is the name of your project?'
-		},
-		{
-			type: 'Select',
-			name: 'frontend',
-			message: 'Choose a frontend framework: ',
-			choices: ['React', 'Angular', 'Vue'],
-			default: 'React'
-		},
-		{
-			type: 'Select',
-			name: 'backend',
-			message: 'Choose a backend framework: ',
-			choices: ['Express', 'Django', 'Flask'],
-			default: 'Express'
-		}
-
-	];
-//Checks for CRTL-C and exits with goodbye message
-	let answers = async () => {
-		try {
-			await prompt(questions)
-		} catch (err) {
-			console.log(chalk.yellow("Closing web-stater-cli..."))
-			shell.exit(0)
-		}
-	}
-
+const main = () => {
+	checkConfig()
 	answers()
+}
+
+main()
+
+
