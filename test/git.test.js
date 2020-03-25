cefe02e20692585633b4c5e926f1a04faf989e75
alexmeier2828 <ameier42@gmail.com>
2020-03-25 19:46:17 -0400

    added test for git

---
+++ test/git.test.js
@@ -0,0 +1,21 @@
+git = require("../src/git.js");
+fs = require("fs")
+
+test("Git Test", async () => {
+  dummy = new git("dummy");
+  var failed = false;
+  try{
+    dummy.init()
+
+    //check if we are successfull
+    await fs.promises.access("./projects/dummy/.git");
+
+
+  }catch(e){
+
+    console.error("failed to initialize git repo - " + e);
+    failed = true;
+  }
+
+  expect(failed).toBe(false);
+});
