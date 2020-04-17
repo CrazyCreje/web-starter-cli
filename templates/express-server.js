6a6addea7c2c31aff71a9ad457c7991d7bf176df
CrazyCreje <cvalenti312@gmail.com>
2020-04-16 20:13:53 -0400

    Add logging to express-server.js, change functions to arrow functions

--- templates/express-server.js
+++ templates/express-server.js
@@ -4,17 +4,18 @@ https://dev.to/loujaybee/using-create-react-app-with-express
 */
 
 const express = require('express');
-const bodyParser = require('body-parser')
 const path = require('path');
 const app = express();
+const port = process.env.PORT || 8080;
+
 app.use(express.static(path.join(__dirname, 'build')));
 
-app.get('/ping', function (req, res) {
+app.get('/ping', (req, res) => {
  return res.send('pong');
 });
 
-app.get('/', function (req, res) {
+app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, 'build', 'index.html'));
 });
 
-app.listen(process.env.PORT || 8080);
+app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`));
