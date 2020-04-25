c05518d8b335ce0a2a5968ab0cffaf3812a09d34
alexmeier2828 <ameier42@gmail.com>
2020-04-25 17:07:33 -0400

    fixed test to look for wsgi.py instead of server.py

--- test/flask.test.js
+++ test/flask.test.js
@@ -11,7 +11,7 @@ test("Flask test", async () => {
     flask.flask(`${working_dir}/testing/flask`);
 
     //check if we are successfull
-    await fs.promises.access("./testing/flask/src/server/server.py");
+    await fs.promises.access("./testing/flask/src/server/wsgi.py");
     await fs.promises.access("./testing/flask/flask-env/bin/activate");
     await fs.promises.access("./testing/flask/requirements.txt");
 
